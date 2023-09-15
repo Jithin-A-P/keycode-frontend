1e9f05409ca9183f4c1c02b5e160afb613a99bd7
Vigneshwar <vigneshwar@keyvalue.systems>
2023-09-15 15:39:31 +0530

    add gaming comments

--- src/pages/Player/index.tsx
+++ src/pages/Player/index.tsx
@@ -5,10 +5,8 @@ import io from 'socket.io-client';
 
 const Player = () => {
   const socket = useRef(null);
-  const [searchParams, setSearchParams] = useSearchParams()
-  // const searchParams = new URLSearchParams(document.location.search);
-  const player = searchParams.get('player'); 
   const [showButtons, setShowButton] = useState(false);
+  const [player, setPlayer] = useState('');
   useEffect(() => {
     socket.current = (io as any).connect('http://192.168.3.186:5050', {
       query: {
@@ -20,7 +18,20 @@ const Player = () => {
     socket.current.on('connect', () => {
       console.log('connected to Game tug of war');
       console.log(socket.current.id); // x8WIv7-mJelg7on_ALbx
-      // socket.emit("from_client", "asd")
+      socket.current.emit("game_request", {
+        gameName:"TUG",
+        gameType: "2p",
+        screenId: "123"
+      }, (data) =>{
+        console.log("invoking game");
+        console.log("game data", data);
+        // if data.status is failure, show some busy message
+        // if suucess, set player
+        if(data.status === 'success'){
+         setPlayer(data.player);
+        }
+        
+      });
     });
     socket.current.on('show_buttons', () => {
       setShowButton(true);
