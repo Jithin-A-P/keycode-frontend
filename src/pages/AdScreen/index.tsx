f1b84382a9ea7d0e9f2eae5d3070a3a91e689d31
Bilta John <bilta@keyvalue.systems>
2023-09-15 13:51:45 +0530

    Media player

--- src/pages/AdScreen/index.tsx
+++ src/pages/AdScreen/index.tsx
@@ -1,5 +1,27 @@
-const AdScreen = () =>(
-<div style={{ width: 100, height: 100, backgroundColor: 'red'}} />
-    )
+
+import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
+import MediaPlayer from './MediaPlayer';
+import styles from './style';
+
+const AdScreen = () =>{
+  const [screenType, setScreenType] = useState('MEDIA_PLAYER');
+
+  const renderScreen = useMemo(()=>{
+    switch(screenType){
+      case 'MEDIA_PLAYER':
+       return <MediaPlayer />
+      default:
+        return  <div />
+    }
+
+  },[screenType]
+  )
+
+
+  return (
+    <div>
+    {renderScreen}
+   </div>
+  )}
 
 export default AdScreen
