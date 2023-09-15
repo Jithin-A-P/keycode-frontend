4c44a0ce2a0b87f84cead0c41cdde7388635de19
Bilta John <bilta@keyvalue.systems>
2023-09-16 01:25:20 +0530

    campaign submission page

---
+++ src/pages/SubmitCampaign/mockdata.ts
@@ -0,0 +1,141 @@
+export const screens = [
+    {
+      "id": "1",
+      "timeslots": [{
+        "id": "1",
+        "screenId": "",
+        "time": 1,
+        "pricePerSecond": 1,
+        "avgViewCount": 200,
+        "liveViewCount": 20
+      }, {
+        "id": "2",
+        "screenId": "",
+        "time": 2,
+        "pricePerSecond": 3,
+        "avgViewCount": 300,
+        "liveViewCount": 10
+      },
+      {
+        "id": "3",
+        "screenId": "",
+        "time": 3,
+        "pricePerSecond": 3,
+        "avgViewCount": 300,
+        "liveViewCount": 10
+      },{
+        "id": "4",
+        "screenId": "",
+        "time": 4,
+        "pricePerSecond": 3,
+        "avgViewCount": 300,
+        "liveViewCount": 10
+      },{
+        "id": "5",
+        "screenId": "",
+        "time": 5,
+        "pricePerSecond": 3,
+        "avgViewCount": 300,
+        "liveViewCount": 10
+      },{
+        "id": "6",
+        "screenId": "",
+        "time": 6,
+        "pricePerSecond": 3,
+        "avgViewCount": 300,
+        "liveViewCount": 10
+      },{
+        "id": "7",
+        "screenId": "",
+        "time": 7,
+        "pricePerSecond": 3,
+        "avgViewCount": 300,
+        "liveViewCount": 10
+      },{
+        "id": "8",
+        "screenId": "",
+        "time": 8,
+        "pricePerSecond": 3,
+        "avgViewCount": 300,
+        "liveViewCount": 10
+      },{
+        "id": "9",
+        "screenId": "",
+        "time": 9,
+        "pricePerSecond": 3,
+        "avgViewCount": 300,
+        "liveViewCount": 10
+      },{
+        "id": "10",
+        "screenId": "",
+        "time": 10,
+        "pricePerSecond": 3,
+        "avgViewCount": 300,
+        "liveViewCount": 10
+      },
+      {
+        "id": "11",
+        "screenId": "",
+        "time": 11,
+        "pricePerSecond": 3,
+        "avgViewCount": 300,
+        "liveViewCount": 10
+      },
+      {
+        "id": "12",
+        "screenId": "",
+        "time": 12,
+        "pricePerSecond": 3,
+        "avgViewCount": 300,
+        "liveViewCount": 10
+      },
+      {
+        "id": "13",
+        "screenId": "",
+        "time": 13,
+        "pricePerSecond": 3,
+        "avgViewCount": 300,
+        "liveViewCount": 10
+      },
+      {
+        "id": "14",
+        "screenId": "",
+        "time": 14,
+        "pricePerSecond": 3,
+        "avgViewCount": 300,
+        "liveViewCount": 10
+      },
+      {
+        "id": "15",
+        "screenId": "",
+        "time": 15,
+        "pricePerSecond": 3,
+        "avgViewCount": 300,
+        "liveViewCount": 10
+      },
+      {
+        "id": "16",
+        "screenId": "",
+        "time": 15,
+        "pricePerSecond": 3,
+        "avgViewCount": 300,
+        "liveViewCount": 10
+      },
+      {
+        "id": "17",
+        "screenId": "",
+        "time": 16,
+        "pricePerSecond": 3,
+        "avgViewCount": 300,
+        "liveViewCount": 10
+      },
+      {
+        "id": "18",
+        "screenId": "",
+        "time": 17,
+        "pricePerSecond": 3,
+        "avgViewCount": 300,
+        "liveViewCount": 10
+      },]
+    },
+  ]
--- src/routes/PrivateLayout.tsx
+++ src/routes/PrivateLayout.tsx
@@ -14,6 +14,7 @@ const AdScreenPage = lazy(() => import('@pages/AdScreen'));
 const GamesPage = lazy(() => import('@pages/Games'));
 const PlayerPage = lazy(() => import('@pages/Player'));
 const Flappy = lazy(() => import('@pages/Games/Flappy'));
+const SubmitCampaign = lazy(() => import('@pages/SubmitCampaign'))
 
 const PrivateLayout = () => {
   const [isBarExpanded, setBarExpanded] = useState(true);
@@ -55,6 +56,10 @@ const PrivateLayout = () => {
               <Route
                 path={RoutePaths.CREATE_CAMPAIGN}
                 element={<CreateCampaign />}
+              />
+              <Route
+                path={RoutePaths.SUBMIT_CAMPAIGN}
+                element={<SubmitCampaign />}
               />
               <Route path={RoutePaths.ALL} element={<PageNotFound />} />
             </Routes>
